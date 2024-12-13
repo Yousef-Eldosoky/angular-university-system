3d9a09a19840bf3880041e69970e3e2e1cd8c6ea
Yousef Eldosoky <eldosoky@yousefsite.com>
2024-12-13 19:13:11 +0200

    remove unnecessary template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,31 +9,12 @@ import { IdentityService } from './identity/identity.service';
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-
-  imports: [RouterModule, MatIconModule, MatButtonModule] ,
-
-
-  template: `
-  <header>
-    <h1>Welcome Back!</h1>
-    <div class="categories">
-      <button class="category" routerLink="/home">dashboard</button>
-      <button class="category" routerLink="/course-management">course management</button>
-      <button class="category" routerLink="/attendance">attendance</button>
-      <button class="category" routerLink="/grading-system">grading system</button>
-      <button class="category" routerLink="/exams">exams</button>
-      <button class="category" routerLink="/QA">QA</button>
-      <button class="category" routerLink="/courses">courses</button>
-    </div>
-  </header>
-  <div>
-    <router-outlet></router-outlet>
-  </div>
-`,
+  imports: [RouterModule, MatIconModule, MatButtonModule],
 })
+
 export class AppComponent {  constructor(private identity: IdentityService) {}
   title = "University System";
-  
+
   ngOnInit(): void {
     this.identity.isLoggedIn();
   }
