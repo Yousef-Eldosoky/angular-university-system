342970b7ca85a780a1caa33e217578d485d6a576
Shahdalaa6 <161372939+Shahdalaa6@users.noreply.github.com>
2024-12-15 00:41:30 +0200

    dashboard update

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,14 +2,23 @@ import { Component } from '@angular/core';
 import { IdentityService } from '../identity/identity.service';
 import { MatIconModule } from '@angular/material/icon';
 import { DecimalPipe } from '@angular/common';
+import { DatePipe } from '@angular/common';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [MatIconModule, DecimalPipe],
+  imports: [MatIconModule, DecimalPipe, DatePipe, CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+  currentDate = new Date();
+
+  events = [
+    { place: 'Conference Room A', time: '10:00 AM - 12:00 PM', guest: ['dr.nabil'] },
+  ];
+
+
   constructor(public identiyService: IdentityService) {}
 }
